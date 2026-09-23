bfabefed87849935fcbfc99d4f94d894c560bced
DiegoFalla-dev <96359361+DiegoFalla-dev@users.noreply.github.com>
2026-09-22 19:17:07 -0500

    init

---
+++ backend/metropolitano-backend-api/src/main/java/metropolitano_backend_api/MetropolitanoBackendApiApplication.java
@@ -0,0 +1,13 @@
+package metropolitano_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetropolitanoBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetropolitanoBackendApiApplication.class, args);
+	}
+
+}
---
+++ backend/metropolitano-backend-api/src/test/java/metropolitano_backend_api/MetropolitanoBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package metropolitano_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetropolitanoBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
